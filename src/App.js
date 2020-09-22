3e6f77480252436a549e97feb8d950a4b3d28fa9
farhadahmed3020 <farhadahmed3020@gmail.com>
2020-09-22 20:23:58 +0600

    user and like

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useState } from 'react';
 
 function App() {
+  const [likeColor, setLikeColor ] = useState('');
+   const  handleLike = () =>{
+    const color = likeColor ? '' : 'primary';
+     setLikeColor(color);
+     setLikeColor(likeColor ? '' : 'primary');
+   }
+   const [users, setUsers] = useState([]);
+   const [singleUser,setSingleUser] =useState({});
+
+   useEffect(() =>{
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => res.json())
+     .then(data => setUsers(data))
+   },[])
+    // single users
+    useEffect(() =>{
+      fetch('https://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.json())
+      .then(data =>setSingleUser(data))
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick={handleLike} color={likeColor}
+      ></ThumbUpAltIcon>
+      <ThumbUpAltIcon onClick={() =>setLikeColor(likeColor ? '' : 'primary')} color ={likeColor}></ThumbUpAltIcon>
+      
+      {
+        users.map(users => <li>{users.name}</li>)
+      }
+      <h1> {singleUser.name}</h1>
+    
+      
     </div>
   );
 }
